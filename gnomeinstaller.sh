c4c5b02e22ff3a600185e83313adfdaced18c1c8
101br03k <59868436+101br03k@users.noreply.github.com>
2020-02-27 21:59:42 +0100

    Update with comments in the code

--- gnomeinstaller.sh
+++ gnomeinstaller.sh
@@ -1,49 +1,34 @@
+#!/bin/bash
+#
 mirror
-
+#
 update
-
+#
 upall
-
+#
 sudo padman -Syyu
-
+#
 git clone https://github.com/arcolinuxd/arco-gnome
-
+#
 cd arco-gnome
-
+#
 sudo ./000-use-all-cores-makepkg-conf-v4.sh
-
 sudo ./100-display-manager-and-desktop-v1.sh
-
 sudo ./110-install-sound-v3.sh
-
 sudo ./120-bluetooth-v2.sh
-
 sudo ./121-fix-bluetooth-switch-not-working-v1.sh
-
 sudo ./130-install-printers-v3.sh
-
 sudo ./140-install-samba-v3.sh
-
 sudo ./150-install-network-discovery-v4.sh
-
 sudo ./160-install-tlp-for-laptops-v1.sh
-
 sudo ./200-software-arch-linux-repo-v2.sh
-
 sudo ./300-software-AUR-repo-v1.sh
-
 sudo ./400-software-arch-linux-repo-distro-specific-v1.sh
-
 sudo ./500-software-AUR-repo-distro-specific-v1.sh
-
 sudo ./600-software-from-ArcoLinux-repo-v1.sh
-
 sudo ./700-installing-fonts-v2.sh
-
 sudo ./800-autologin-v3.sh
-
 sudo ./900-fix-microcode-error-v1.sh
-
 sudo ./910-fix-mouse-cursor-breeze-snow-v1.sh
-
 sudo reboot
+#reboots the system
