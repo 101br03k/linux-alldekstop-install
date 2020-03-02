13511d8b55902980fc356ba0a64b2845793fcd44
A3 <59868436+101br03k@users.noreply.github.com>
2020-03-02 13:30:55 +0100

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome, 2) plasma, 3) budgie"
 read dekstop
 echo what is your username
