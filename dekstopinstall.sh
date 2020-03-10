20a69c2a5837576421accb7b85834a76d5728fc9
A3 <59868436+101br03k@users.noreply.github.com>
2020-03-10 15:30:48 +0100

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome, 2) plasma, 3) budgie 4) bspwm 5) budgie 6) cinnamon"
 read dekstop
-echo what is your username
-read username
-echo what is your password
-stty -echo 
-read password
-stty echo
-echo "do you want to autologin (if you want to use autologin type yes, otherwise type no)"
-read autologin
-#username and password for automatic setting up a gnome account
 
-mirror
-yes | update
-yes $password | sudo mirror
-#answers yes to proceed with update / installation and automatic types in your password to proceed with instalation
-upall
 
 if ($dekstop = 1 || $dekstop = gnome || dekstop = 1 gnome){
 git clone https://github.com/arcolinuxd/arco-gnome
 cd arco-gnome
+./bash gnome
 }
+
 else if ($dekstop = 2 || $dekstop = plasma || $dekstop = 2 plasma){
 git clone https://github.com/arcolinuxd/arco-plasma
 cd arco-plasma
 }
+
 else if ($dekstop = 3 || $dekstop = awasome || $dekstop = 3 awasome){
 git clone https://github.com/arcolinuxd/arco-awasome
 cd arco-plasma
 }
+
 else if ($dekstop = 4 || $dekstop = bspwm || $dekstop = 4 bspwm){
 git clone https://github.com/arcolinuxd/arco-bspwm
 cd arco-bspwm
 }
+
 else if ($dekstop = 5 || $dekstop = budgie || $dekstop = 5 bugie){
 git clone https://github.com/arcolinuxd/arco-budgie
 cd arco-budgie
 }
+
 else if ($dekstop = 7 || $dekstop = cinnamon || $dekstop = 6 cinnamon){
 git clone https://github.com/arcolinuxd/arco-awasome
 cd arco-plasma
