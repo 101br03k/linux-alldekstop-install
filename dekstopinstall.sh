6ee6b471dddafba90e5c8619098ac05fd165c9ee
A3 <59868436+101br03k@users.noreply.github.com>
2020-03-12 16:03:10 +0100

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -3,7 +3,7 @@ echo "welcome to my script, please choose wich dekstop you want to install, you 
 echo "please type the name of the number of the dekstop you want to install"
 read dekstop
 
-if (dekstop = 1 | dekstop = gnome | dekstop = 1 gnome)
+if ($dekstop = 1 | $dekstop = gnome | $dekstop = 1 gnome)
 {
 sudo bash gnome.sh
 }
