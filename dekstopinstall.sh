5475c2b15d8e2db0e0bb495a395195e623f36fb8
A3 <59868436+101br03k@users.noreply.github.com>
2020-03-12 16:00:58 +0100

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome, 2) plasma, 3) budgie 4) bspwm 5) budgie 6) cinnamon"
-echo "please type in your terminal / commmand line: sudo bash (name of your dekstop).sh"
+echo "please type the name of the number of the dekstop you want to install"
+read dekstop
+
+if (dekstop = 1 | dekstop = gnome | dekstop = 1 gnome)
+{
+sudo bash gnome.sh
+}
