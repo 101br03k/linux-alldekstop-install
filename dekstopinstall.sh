f44bf22e38745c28a5d363f8b4cd37568092a246
A3 <59868436+101br03k@users.noreply.github.com>
2020-03-27 12:18:29 +0100

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome, 2) plasma, 3) budgie 4) bspwm 5) budgie 6) cinnamon"
-echo "please type the name of the number of the dekstop you want to install"
+echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome.sh"
+echo "please type in wich dekstop you want to install, please type the name correct, or this will fail"
 read dekstop
-
-if ($dekstop = 1 | $dekstop = gnome | $dekstop = 1 gnome)
-{sudo bash gnome.sh}
-else if ($dekstop = 2 | $dekstop = plasma | $dekstop = 2 plasma)
-{sudo bash gnome.sh}
-else if ($dekstop = 3 | $dekstop = budgie | $dekstop = 3 budgie)
-{sudo bash gnome.sh}
-else if ($dekstop = 4 | $dekstop = bspwm | $dekstop = 4 bspwm)
-{sudo bash gnome.sh}
+sudo bash $dekstop
