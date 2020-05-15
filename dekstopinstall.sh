5e66a2fe1371bfa435247bb0072eb42d042f89ad
A3 <59868436+101br03k@users.noreply.github.com>
2020-05-15 11:12:34 +0200

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome.sh"
+echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome.sh 2) plasma.sh"
 echo "please type in wich dekstop you want to install, please type the name correct, or this will fail"
 read dekstop
 sudo bash $dekstop
