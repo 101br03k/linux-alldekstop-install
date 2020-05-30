bb90352e53d1d82edf4f34629a36e99b07fccd23
A3 <59868436+101br03k@users.noreply.github.com>
2020-05-30 09:25:07 +0200

    Update dekstopinstall.sh

--- dekstopinstall.sh
+++ dekstopinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -x
 echo "welcome to my script, please choose wich dekstop you want to install, you can choose between: 1) gnome.sh 2) plasma.sh" 3) qitle.sh
 echo "please type in wich dekstop you want to install, please type the name correct, or this will fail"
 read dekstop
